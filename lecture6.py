344a4fc0a154e5954b91d4ab25ec78dbafae7b10
Anshul savita <anshulsavita370@gmail.com>
2026-05-29 22:20:47 +0530

    added lecture 6 where while topic have been covered

---
+++ lecture6.py
@@ -0,0 +1,56 @@
+# ****while loop
+# while(exp):
+#     =====
+#     =====
+#     =====
+    # [break]
+    # [continue]
+
+# i=1
+# while(i<=5):
+#     print(i,end=",")
+#     i=i+1
+
+# n=int(input("Enter a number:"))
+# i=1
+# while(i<=10):
+#     t=n*i
+#     print(n,"X",i,"=",t)
+#     i+=1
+
+# i=1
+# while(i<=5):
+#     j=1
+#     while(j<=5):
+#         print(j,end=' ')
+#         j+=1
+#     print()
+#     i+=1
+
+# i=1
+# while(i<=5):
+#     j=2
+#     while(j<=8):
+#         print(j,end=' ')
+#         j+=2
+#     print()
+#     i+=1
+
+# i=1
+# while(i<=5):
+#     j=1
+#     while(j<=i):
+#         print(j,end=' ')
+#         j+=1
+#     print()
+#     i+=1
+
+# i=5
+# while(i>=1):
+#     j=1
+#     while(j<=i):
+#         print(j,end=' ')
+#         j+=1
+#     print()
+#     i-=1
+
