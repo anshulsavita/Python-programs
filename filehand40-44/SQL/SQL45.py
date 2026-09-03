31675b06e2d3eb62a4521b832a7fe89fc817fc65
Anshul savita <anshulsavita370@gmail.com>
2026-09-03 11:06:05 +0530

    added what is Sql in lecture 45

---
+++ filehand40-44/SQL/SQL45.py
@@ -0,0 +1,40 @@
+'''
+DBMS --> Database management system (data + base)
+data --> collection of raw facts and figures
+
+      A(student Name)                B(Maths) --> (column name / attributes / field)
+      Tony                           34
+----------
+field --> one piece of information related to an object known as field
+----------
+Ex --> accoun number, customuer name, aadhar number
+----------
+record/row --> collection of fields
+----------
+database --> collection of records
+----------
+management system -->
+    => append the records
+    => searching
+    => sorting
+    => delete records
+    => update the records
+
+database example 
+- foxpro
+- foxbase 
+- dbase
+
+RDBMS=  relational database management system
+==>  a russian mathmatician scientist MR CODD develop 13 rules of RDBMS.
+==>  jo bhi data ye 13 rules follow karege woh RDBMS hoga.
+
+systems--
+-> ms sql server
+-> My sql
+-> postgress sql
+-> oracle
+-> ingress, unify
+-> mongodb/ firebase
+-> no sql
+'''
