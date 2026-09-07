50988d2c0dc2a8933480706a1f03bdfbbff605da
Anshul savita <anshulsavita370@gmail.com>
2026-09-07 15:56:07 +0530

    added some workd meanings in sql

--- SQL/MySQL25.py
+++ SQL/MySQL25.py
@@ -1,9 +1,22 @@
 """
+word meanings-->
+---------------------------------------------------------
+ALTER → Changes the structure of an existing table.
+DROP → Completely deletes a table/database.
+TRUNCATE → Deletes all data from a table but keeps the table structure.
+GRANT → Gives permissions to a user.
+REVOKE → Removes permissions from a user.
+COMMIT → Permanently saves changes.
+ROLLBACK → Undoes changes that haven't been committed.
+SAVEPOINT → Creates a point you can roll back to later within a transaction.
+Retrieve data → get data from the database.
+------------------------------------------------------------
 Schema --> Group of tables (Database)
     - inside Schema we create tables
 Table --> A table is a structure that stores data in rows and columns, like a spreadsheet.
 
 SQL --> Structured Quiry Language
+    -SQL is a language used to store, retrieve, and manage data in databases.
 --------------------
 Types of SQl -->
     --DDL (Data Definition Language.)
@@ -18,6 +31,15 @@ Types of SQl -->
         => User Creation
         => Grant/Revoke permission for Table & Schema's.
 
+    --DQL (Data Query Language.) -->(*We will study it later)
+        => Retrieves data
+        => Search data
+
+    --TCL (Transaction Control Language.) -->(*We will study it later)
+        => Commit
+        => RollBack
+        => SavePoint
+
 ******DDL******
 ---------------
     - Create Schema/database.
@@ -30,7 +52,7 @@ Types of SQl -->
 
     - constraints --> 
         - Primary Key --> Data Should be unique & not null.
-        - Unique --> Data should be unique but alloew null entries.
+        - Unique --> Data should be unique but allow null entries.
         - not null --> Null/Blank entries not allowed
         - null(default) --> Allow Null entries
         - auto_increment --> Will generate primary key implicitly.
