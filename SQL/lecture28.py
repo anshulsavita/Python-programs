3eb28e8697f1010b1bf2a524ea815f84bbc9c293
Anshul savita <anshulsavita370@gmail.com>
2026-09-12 15:34:49 +0530

    added lecture 28 of mySql where select query, aggregate function, having and group by has been covered

--- SQL/lecture28.py
+++ SQL/lecture28.py
@@ -21,17 +21,21 @@ select * from employees order by employeename
 
 select * from employees order by employeename desc
     --> will print employeename in descinding order
-
-**** Aggregate function ****** 
+------------------------------------------------
+**** Aggregate function ****** --> mainly work with group
 min(col)
-    --> will print minimum values
+    --> will print minimum values in a group
 max(col)
-    --> will print maximum values
+    --> will print maximum values in a group
 sum(col) 
-    --> will print sum 
-avg(col) -->not include null
-count(*) --> count null also
-
+    --> will print sum of all the numbers in a coloum of a group
+avg(col) 
+    --> will print average value of a group, not include null
+count(col) 
+    --> will count number of entries in a coloum, will not count null entries
+count(*)
+    --> will count number of entries in a coloum, will count null entries also
+-------------------------------------------------
 select max(salary) from employees
     --> print max salary from all the employees
 select min(salary) from employees
@@ -45,8 +49,8 @@ select count(salary) from employees
     --> it will count number of employees who get salary, null entries will not be count
 select count(*) from employees
     --> it will count number of employees, null entries will not be count
-*
-*************************************************
+
+------------------------------------------------
 
 select count(*) as 'Male_employees' from employees where gender = 'Male'
     - count how many male employee are in our company
@@ -71,26 +75,25 @@ select city,count(*),sum(salary) from employees group by city
 
 select city,gender,count(*),sum(salary) from employees group by city,gender
     - For every unique city-gender combination, count employees and calculate their total salary.
-
+------------
+having -->
+-----------
 select city,gender,count(*),sum(salary) from employees group by city,gender
 having sum(salary)>500000
     - which group we gave salary more than 500000
-
-
-    
-
-
-
-
-
-fild names
-customer ID / CompanyName / ContactTitle / Address / Region / postalCode / Country / Phone /
+    - with having (like) will not work, >,<,+,-... they will work
+------------------------------------------------------------------------
 
 select * from customers where region is null
+    --> will show data whode region(this is coll) is null
 select * from customers where region is not null
+    --> will show data whose region is not null
 
+select count(*) from customers where contactTitle='Sales Representative'
+    --> it will count total no of sales represebtative
 
-
+select country,count(*) from customers where country in ('USA','Brazil') group by country
+    --> will count how many customers are from usa and brazil
 
 
 
