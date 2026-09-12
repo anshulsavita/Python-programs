db7b0f86af2f6e05bb9b1fb3823fb40bfb826c30
Anshul savita <anshulsavita370@gmail.com>
2026-09-12 13:56:30 +0530

    added lecture 28 of mysql

---
+++ SQL/lecture28.py
@@ -0,0 +1,111 @@
+'''
+syntax-->
+    - select [*]/[<colname>,<colname>.....] from <table>
+
+    - [where <condition>]
+    - [where <colname> [not] like 'pattern%_']
+    - [where <colname> [not] between v1 and v2]  --> v1 & v2 represent value1 & value
+    - [where <colname> [not] in (v1,v2,v3,v4)]
+    - [order by <coll>,<col2>....[desc]]
+    - [group by <col1>,<col2>....]
+    - [having <condition>]
+
+select * from employees where salary between 10000 to 20000
+    --> will print data whose salary between 10000 and 20000, 10000,20000 will also be included.
+
+select * from employees where dob between '2000-1-1' to '2026-12-31'
+    --> will print data
+
+select * from employees order by employeename
+    --> will print employeename in asscending order
+
+select * from employees order by employeename desc
+    --> will print employeename in descinding order
+
+**** Aggregate function ****** 
+min(col)
+    --> will print minimum values
+max(col)
+    --> will print maximum values
+sum(col) 
+    --> will print sum 
+avg(col) -->not include null
+count(*) --> count null also
+
+select max(salary) from employees
+    --> print max salary from all the employees
+select min(salary) from employees
+    --> print min salary
+select sum(salary) from employees
+    --> will print sum of all the salaries
+select avg(salary) from employees
+    --> will print avegrage salary 
+
+select count(salary) from employees
+    --> it will count number of employees who get salary, null entries will not be count
+select count(*) from employees
+    --> it will count number of employees, null entries will not be count
+*
+*************************************************
+
+select count(*) as 'Male_employees' from employees where gender = 'Male'
+    - count how many male employee are in our company
+
+select sum(salary) from employees where gender = 'Male'
+    - how much salary we gave to our male employees
+
+select employeename, sum(salary) from employees where gender = 'Male'
+    - here we are performing a multicolumn result(employeename) with a single column result [sum(saalary)] which is a wrong method to perform never do this.
+
+-----------------------------------------------
+group by -->
+-----------
+select city from employees group by city 
+    --> it will make grop of city and show every city one time.
+
+select city,count(*) from employees group by city 
+    --> it will print city and how many employees are from there.
+    
+select city,count(*),sum(salary) from employees group by city 
+    --> it will print city and how many employees are from there and sum of ther salary.
+
+select city,gender,count(*),sum(salary) from employees group by city,gender
+    - For every unique city-gender combination, count employees and calculate their total salary.
+
+select city,gender,count(*),sum(salary) from employees group by city,gender
+having sum(salary)>500000
+    - which group we gave salary more than 500000
+
+
+    
+
+
+
+
+
+fild names
+customer ID / CompanyName / ContactTitle / Address / Region / postalCode / Country / Phone /
+
+select * from customers where region is null
+select * from customers where region is not null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
